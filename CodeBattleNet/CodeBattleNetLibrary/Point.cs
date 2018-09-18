1b0958b287d35d8da10a0dc2f5c9b55be8b2bb4e
Andrei_Dorodov <andrew.1992.g@gmail.com>
2018-09-18 17:21:15 +0400

    added client .net

---
+++ CodeBattleNet/CodeBattleNetLibrary/Point.cs
@@ -0,0 +1,16 @@
+﻿namespace CodeBattleNetLibrary
+{
+    public class Point
+    {
+        public int X { get; }
+        public int Y { get; }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString() => "[" + X + "," + Y + "]";
+    }
+}
